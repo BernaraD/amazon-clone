e348ad07ef0db321c619fa3f4a5ba25c957db65f
Bernara Dzhaparova <57506492+BernaraD@users.noreply.github.com>
2021-04-01 17:29:43 -0400

    Order page completed

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,7 +3,10 @@ import axios from "axios";
 
 const instance = axios.create({
     //THE API ( cloud function ) URL
-    baseURL: 'http://localhost:5001/clone-4283f/us-central1/api'
+    baseURL: 'https://us-central1-clone-4283f.cloudfunctions.net/api'
+
+
+   //Local link  'http://localhost:5001/clone-4283f/us-central1/api'
 
 
 });
